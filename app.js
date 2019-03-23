86c9e75325980f29b5395487a658d723aa730d5f
nicholas <nicholasdalrymplealf@carleton.cmail.ca>
2019-03-22 23:10:14 -0400

    final commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const socketIO  = require('socket.io')(server); //hello I am new
 
 const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved/blocked by firewall ...
 
+var playerOne = 0;
+var playerTwo = 0;
+
 app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
 //routes
@@ -21,6 +24,7 @@ app.get('/controller', function(req,res) {
 socketIO.on('connection', function(socket) {
     console.log(socket.id + ' has connected!');
 
+
     socket.on('disconnect', function(data) {
         console.log(socket.id + ' has disconnected');
     });
@@ -28,21 +32,72 @@ socketIO.on('connection', function(socket) {
     //custom events
     //socket = one client
     //socketIO.sockets = all clients
+
+    socket.on('playerOne', function(data) {
+        if(playerOne != 100 & playerTwo != 100){
+            playerOne ++;
+            socketIO.sockets.emit('Score', [playerOne, playerTwo]);
+        }
+        else if(playerOne == 100){
+            socketIO.sockets.emit('Win', "playerOne");
+        }
+        else if(playerTwo == 100){
+            socketIO.sockets.emit('Win', "playerTwo");
+        }
+    });
+
+    socket.on('playerTwo', function(data) {
+        if(playerOne != 100 & playerTwo != 100){
+            playerTwo ++;
+            socketIO.sockets.emit('Score', [playerOne, playerTwo]);
+        }
+        else if(playerOne == 100){
+            socketIO.sockets.emit('Win', "playerOne");
+        }
+        else if(playerTwo == 100){
+            socketIO.sockets.emit('Win', "playerTwo");
+        }
+    });
+
+
     socket.on('red', function(data) {
+        var buttonOn = "red1";
         console.log('red event heard');
-        socketIO.sockets.emit('color_change', {r:255, g:0, b:0});
+        socketIO.sockets.emit('color_change', buttonOn);
     });
 
     socket.on('green', function(data) {
+        var buttonOn = "green1";
         console.log('green event heard');
-        socketIO.sockets.emit('color_change', {r:0, g:255, b:0});
+        socketIO.sockets.emit('color_change', buttonOn);
     });
 
     socket.on('blue', function(data) {
+        var buttonOn = "blue1";
         console.log('blue event heard');
-        socketIO.sockets.emit('color_change', {r:0, g:0, b:255});
+        socketIO.sockets.emit('color_change', buttonOn);
+    });
+
+    socket.on('red2', function(data) {
+        var buttonOn = "red2";
+        console.log('red2 event heard');
+        socketIO.sockets.emit('color_change', buttonOn);
+    });
+
+    socket.on('green2', function(data) {
+        var buttonOn = "green2";
+        console.log('green2 event heard');
+        socketIO.sockets.emit('color_change', buttonOn);
+    });
+
+    socket.on('blue2', function(data) {
+        var buttonOn = "blue2";
+        console.log('blue2 event heard');
+        socketIO.sockets.emit('color_change', buttonOn);
     });
 });
+
+
 
 //finally, start server
 server.listen(LISTEN_PORT);
